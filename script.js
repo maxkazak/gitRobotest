d786dfe6d56f9db9c57d43e40e46e64f68924fd4
maxkazak <max-kazak@yandex.ru>
2018-01-25 15:47:15 +0300

    v 0.3 - factorial added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert(hello world);
+var res=1;
+factorial(6);
+function factorial(n){
+	res*=n;
+	n>1?factorial(n-1):print(res);
+}
+function print(string){
+	document.getElementById("output").innerHTML=string;
+}
